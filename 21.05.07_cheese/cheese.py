8bfed79786f77bf4239090bd16af567148064eeb
Eggsy <nohyoguy7@gmail.com>
2022-05-07 02:14:07 +0900

    cheese problem v1.1

--- 21.05.07_cheese/cheese.py
+++ 21.05.07_cheese/cheese.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 f = open("fucking_cheese.txt", 'r')
-while True:
-    line = f.readline()
-    if not line:
-        break
-    print(line[:-1])
+lines = f.readlines()
 f.close()
+
+width = len(lines[0][:-1])
+hight = len(lines)
+
+cheese_list = []
+cell_list = []
+for i in range(width+2):
+    cell_list.append('3')
+cheese_list.append(cell_list)
+
+for i in range(hight):
+    cell_list = ['3']
+    for j in range(width):
+        cell_list.append(lines[i][j])
+    cell_list.append('3')
+    cheese_list.append(cell_list)
+
+cheese_list.append(cheese_list[0])
+
+for component in cheese_list:
+    print(component)
