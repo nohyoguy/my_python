94d706d9ce46118f2809d1d9eae6ac2925093385
Eggsy <nohyoguy7@gmail.com>
2022-05-07 01:44:50 +0900

    file in out update

--- 21.05.07_cheese/cheese.py
+++ 21.05.07_cheese/cheese.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-f = open("새파일.txt", 'w')
-for i in range(1, 11):
-    data = "%d번째 줄입니다.\n" % i
-    f.write(data)
+f = open("fucking_cheese.txt", 'r')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(line[:-1])
 f.close()
--- 21.05.07_file_in_out/file_in_out.py
+++ 21.05.07_file_in_out/file_in_out.py
@@ -4,3 +4,11 @@ for i in range(1, 11):
     data = "%d번째 줄입니다.\n" % i
     f.write(data)
 f.close()
+
+f = open("새파일.txt", 'r')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(line[:-1])
+f.close()
